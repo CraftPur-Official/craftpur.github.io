fb03d2a34a59e47948b816603127805b5448761a
CraftPur-Official <noreply.craftpur@gmail.com>
2023-04-26 23:23:06 +0530

    commit

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-function increaseLikeCount(element) {
-    const nextElement = element.nextElementSibling
-    if(element.classList.contains('fa-solid'))
+function increaseLikeCount(like) {
+    const nextElement = like.nextElementSibling
+    const rankElement = like.parentNode.previousElementSibling.previousElementSibling
+    const rankElementStyle = window.getComputedStyle(rankElement)
+    const nextElementStyle = window.getComputedStyle(nextElement)
+
+    if(like.classList.contains('fa-solid'))
     {
-        element.classList.remove('fa-solid')
-        element.classList.add('fa-regular')
+        like.classList.remove('fa-solid')
+        like.classList.add('fa-regular')
         nextElement.innerText = 'Like'
         nextElement.style.fontWeight = '400'
+        like.style.color = nextElementStyle.color;
+        
     }
     else {
-        element.classList.add('fa-solid')
-        element.classList.remove('fa-regular')
+        like.classList.add('fa-solid')
+        like.classList.remove('fa-regular')
         nextElement.innerText = 'Liked!'
-        nextElement.style.fontWeight = '500'
+        nextElement.style.fontWeight = '500';
+        like.style.color = rankElementStyle.color
     }
 }
