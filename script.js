21f07715578d8b75fe2c7867c84d40a0f3a89407
CraftPur-Official <noreply.craftpur@gmail.com>
2023-04-26 20:53:24 +0530

    commit member update

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 function increaseLikeCount(element) {
+    const nextElement = element.nextElementSibling
     if(element.classList.contains('fa-solid'))
     {
         element.classList.remove('fa-solid')
         element.classList.add('fa-regular')
-        var count = document.getElementsByClassName(element.classList.cont)
-        
+        nextElement.innerText = 'Like'
+        nextElement.style.fontWeight = '400'
     }
     else {
         element.classList.add('fa-solid')
         element.classList.remove('fa-regular')
+        nextElement.innerText = 'Liked!'
+        nextElement.style.fontWeight = '500'
     }
 }
